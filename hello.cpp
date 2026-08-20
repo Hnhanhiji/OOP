a87d472f90d7fcf0dc9d9ad61db357612bfddfa1
Hnhanhiji <bhieunhan@gmail.com>
2026-08-20 17:45:05 +0700

    init project

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Xin chao! Day la mot chuong trinh C++ don gian." << endl;
+    cout << "Toi la AI ho tro ban trong VS Code." << endl;
+    return 0;
+}
