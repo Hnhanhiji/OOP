d5fa3e233e5a91fe8cc55978aef4a86274c663a1
Hnhanhiji <bhieunhan@gmail.com>
2026-09-09 07:17:28 +0700

    Update HW09.cpp

--- Week4/HW09.cpp
+++ Week4/HW09.cpp
@@ -1,1 +1,25 @@
+// cau9.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+//  Tính lũy thừa x^n 
+void luythua(int x, int n)
+{
+	int result = 1;
+	for (int i = 0; i < n; i++)
+	{
+		result *= x;
+	}
+	cout << "Luy thua cua " << x << "^" << n << " = " << result << endl;
+}
+
+
+
+int main()
+{
+    luythua(2, 3);
+    return 0;
+}
+
 
