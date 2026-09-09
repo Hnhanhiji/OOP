26607a289cdf0277bb8a7f200cb74077e6e65dec
Hnhanhiji <bhieunhan@gmail.com>
2026-09-09 07:16:48 +0700

    Update HW08.cpp

--- Week4/HW08.cpp
+++ Week4/HW08.cpp
@@ -1,1 +1,37 @@
+// cau8.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+//  Bội chung nhỏ nhất của n số nguyên
+void bcnn(int n)
+{
+	int a[100];
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Nhap so thu " << i + 1 << ": ";
+		cin >> a[i];
+	}
+	int bcnn = a[0];
+	for (int i = 1; i < n; i++)
+	{
+		int temp = bcnn;
+		while (temp % a[i] != 0)
+		{
+			temp += bcnn;
+		}
+		bcnn = temp;
+	}
+	cout << "Boi chung nho nhat cua " << n << " so nguyen la: " << bcnn << endl;
+}
+
+int main()
+{	
+    int n;
+    cout << "Nhap so luong so nguyen: ";
+    cin >> n;
+    bcnn(n);
+    return 0;
+}
+
 
