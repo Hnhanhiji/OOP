10fb17a94d5262ae605dd06403edb58578c5120a
Hnhanhiji <bhieunhan@gmail.com>
2026-09-09 09:09:05 +0700

    Update main.cpp

--- Week5/main.cpp
+++ Week5/main.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int numStudents;
+
+    
+    while (true) {
+        cout << "How many students? ";
+        cin >> numStudents;
+
+        // Kiểm tra xem số lượng học sinh có hợp lệ không (2 <= N <= 20)
+        if (numStudents >= 2 && numStudents <= 20) {
+            break;
+        }
+
+       
+        cout << "Invalid number of students!" << endl;
+        cout << "Please enter a number from 2 to 20." << endl;
+        cout << endl; 
+    }
+
+   
+
+    return 0;
+}
