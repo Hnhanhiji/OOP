15685a13a8bbf0c38fe29d8bb7ea584555b0d39d
Hnhanhiji <bhieunhan@gmail.com>
2026-09-09 07:12:53 +0700

    Implement perfect number check in HW03.cpp
    Add functionality to check if a number is perfect.

--- Week4/HW03.cpp
+++ Week4/HW03.cpp
@@ -1,1 +1,34 @@
+// cau3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+// Kiểm tra n có phải số hoan hảo hay không?
+
+int main()
+{
+    int n;
+    cout << "Nhap so n: ";
+    cin >> n;
+
+    int sum = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            sum += i;
+        }
+    }
+
+    if (sum == n)
+    {
+        cout << n << " la so hoan hao.\n";
+    }
+    else
+    {
+        cout << n << " khong phai la so hoan hao.\n";
+    }
+
+    return 0;
+}
 
