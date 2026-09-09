f28b7c965e4ea1cc72892268788ee97629b52809
Hnhanhiji <bhieunhan@gmail.com>
2026-09-09 07:12:06 +0700

    Implement factorial calculation in HW02.cpp

--- Week4/HW02.cpp
+++ Week4/HW02.cpp
@@ -1,1 +1,23 @@
+// cau2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+// tính n!
+
+int main()
+{ 
+    int n;
+    cout << "Nhap n: ";
+    cin >> n;
+    int giaiThua = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        giaiThua *= i;
+    }
+    cout << n << "! = " << giaiThua << endl;
+    return 0;
+}
+
+
 
