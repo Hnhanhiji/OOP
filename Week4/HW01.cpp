f77b9f8a4470221817049b9c2f455cb0d85c5718
Hnhanhiji <bhieunhan@gmail.com>
2026-09-09 07:11:14 +0700

    Implement program to find the largest of three numbers

--- Week4/HW01.cpp
+++ Week4/HW01.cpp
@@ -1,1 +1,26 @@
+// cau1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+// tìm số lớn nhất trong 3 chữ số a,b,c
+
+
+int main()
+{
+    int a, b, c;
+    cout << "Nhap 3 so a, b, c: ";
+    cin >> a >> b >> c;
+
+    int max = a;
+    if (b > max)
+        max = b;
+    if (c > max)
+        max = c;
+
+    cout << "So lon nhat la: " << max << endl;
+
+    return 0;
+}
+
 
