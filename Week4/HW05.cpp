7cee6a007e8f1c3df639e3b2a35868429493425b
Hnhanhiji <bhieunhan@gmail.com>
2026-09-09 07:14:35 +0700

    Update HW05.cpp

--- Week4/HW05.cpp
+++ Week4/HW05.cpp
@@ -1,1 +1,28 @@
+// cau5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+//  Đảo ngược số
+
+
+int main()
+{   
+    int n, soDaoNguoc = 0;
+
+    cout << "Nhập vào một số nguyên: ";
+    cin >> n;
+
+    while (n > 0) {
+        soDaoNguoc = soDaoNguoc * 10 + n % 10;
+        n /= 10;
+    }
+
+    cout << "Số đảo ngược là: " << soDaoNguoc << endl;
+    return 0;
+}
+
+
+
+
 
