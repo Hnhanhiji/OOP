2eea407d0f293bf9d8163d0704b2ed02441b5cb4
Hnhanhiji <bhieunhan@gmail.com>
2026-09-09 07:13:40 +0700

    Add function to check if a number is symmetric

--- Week4/HW04.cpp
+++ Week4/HW04.cpp
@@ -1,1 +1,36 @@
+// cau4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+//  Kiểm tra n có phải là số đối xứng hay không?
+bool isSymmetric(int n) {
+	int original = n;
+	int reversed = 0;
+	while (n > 0) {
+		int digit = n % 10;
+		reversed = reversed * 10 + digit;
+		n /= 10;
+	}
+	return original == reversed;
+}
+int main()
+{   
+	int n;
+
+	cout << "Nhap so n: ";
+	cin >> n;
+
+	if (isSymmetric(n)) {
+		cout << n << " la so doi xung." << endl;
+	}
+	else {
+		cout << n << " khong phai la so doi xung." << endl;
+	}
+
+ 
+}
+
+
 
