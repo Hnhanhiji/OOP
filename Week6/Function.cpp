83b9c90126f4acd7ac4df705c74a3bf55e0b6d9b
Hnhanhiji <bhieunhan@gmail.com>
2026-09-11 10:12:07 +0700

    Update Function.cpp

--- Week6/Function.cpp
+++ Week6/Function.cpp
@@ -1,1 +1,32 @@
+// Function.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+// void showelcome
+void  showWelcome()
+{
+	cout << "==============================\n";
+	cout << "Welcome to Student Manager\n";
+	cout << "==============================\n";
+}
+// void showMenu include display Students, Show statistics, exit
+void showMenu()
+{
+	cout << "1. Display Students\n";
+	cout << "2. Show Statistics\n";
+	cout << "3. Exit\n";
+}
+
+
+int main()
+{	// call the fucntion showWelcome
+	showWelcome();
+    showMenu();
+
+	cout << "Let's get started!\n";
+	return 0;
+}
+
+
 
