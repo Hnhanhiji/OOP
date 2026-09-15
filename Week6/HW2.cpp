05d9610394905ddd7ac768159ccaee107caf7d73
Hnhanhiji <bhieunhan@gmail.com>
2026-09-16 06:33:17 +0700

    Update HW2.cpp

--- Week6/HW2.cpp
+++ Week6/HW2.cpp
@@ -11,3 +11,12 @@
         printHeader();
         printFlower(flowers[maxIdx]);
     }
+// Task 5: Tinh tong so luong hoa
+void task5_calculateTotalQuantity(Flower flowers[], int N) {
+    cout << "\n--- Task 5: Tong so luong hoa ---\n";
+    int total = 0;
+    for (int i = 0; i < N; i++) {
+        total += flowers[i].quantity;
+    }
+    cout << "Tong so luong tat ca cac bong hoa: " << total << endl;
+}
