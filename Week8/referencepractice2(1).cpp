6b4ae75c9a48aafdeac8e1e84811450596f79120
Hnhanhiji <bhieunhan@gmail.com>
2026-09-18 09:55:28 +0700

    Update and rename referencepractice2.cpp to referencepractice2(1).cpp

---
+++ Week8/referencepractice2(1).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// Hàm hoán đổi giá trị sử dụng tham chiếu (reference)
+void swapValues(int& x, int& y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+int main() {
+    int a = 5, b = 10;
+    
+    cout << "Before: a = " << a << ", b = " << b << endl;
+    
+    // Gọi hàm hoán đổi
+    swapValues(a, b);
+    
+    cout << "After: a = " << a << ", b = " << b << endl;
+    
+    return 0;
+}
+
