b882eddb9207320edca780d856f9933e1cf4b4a3
Hnhanhiji <bhieunhan@gmail.com>
2026-09-18 10:06:35 +0700

    Update practice3.cpp

--- Week8/practice3.cpp
+++ Week8/practice3.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 3;
+    int& y = x; // y là biến tham chiếu đến x (hai biến dùng chung một ô nhớ)
+    x = 7;
+    y = 2;
+    
+    cout << x << " " << y << endl;
+    return 0;
+}
 
