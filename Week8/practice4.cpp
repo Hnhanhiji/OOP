310c81ca9f17a1b653d3daee9eb986b065d510c6
Hnhanhiji <bhieunhan@gmail.com>
2026-09-18 10:09:07 +0700

    Update practice4.cpp

--- Week8/practice4.cpp
+++ Week8/practice4.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// Truyền tham trị (pass by value)
+void changeValue(int x) {
+    x = 100;
+}
+
+// Truyền tham chiếu (pass by reference)
+void changeRef(int& x) {
+    x = 200;
+}
+
+int main() {
+    int a = 5;
+    
+    changeValue(a);
+    cout << "After changeValue: " << a << endl;
+    
+    changeRef(a);
+    cout << "After changeRef: " << a << endl;
+    
+    return 0;
+}
 
